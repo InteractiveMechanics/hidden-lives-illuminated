fff390fc30422147fe3f53f940665c8cba90ff29
Michael Tedeschi <mike@interactivemechanics.com>
2019-06-25 19:39:07 -0400

    Update films page

--- films/index.php
+++ films/index.php
@@ -14,87 +14,132 @@
 			<div class="page-content clearfix">
 				<h1><?php echo $page_title; ?></h1>
 				
+				
 				<br /><br />
-				<h3>Filmmakers at Philadelphia’s Riverside Correctional Facility for Women <small>You Can Go Left or Right</small></h3>
-				<p>A collaborative film reflecting on life inside a hectic county jail for women.</p>
-				<div class="clearfix"></div><br/>
+				<div class="text-center">
+					<h2 class="text-underline">Week 1: The View from Inside</h2>
+					<h2>August 15 to August 21, 2019</h2>
+				</div>
+
 				
-				<h3>Ezra B. <small>Phoenix</small></h3>
-				<p>After a life behind bars, the filmmaker reflects on cultivation of wisdom and the strength to overcome. “I am the Phoenix”</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Lewis B. <small>Money Talks</small></h3>
-				<p>It don’t grow on trees. How much is money really worth?</p>
+				<br /><br />
+				<h3>Aaron W. <small>Where Did It Go?</small></h3>
+				<p>There are many twists and turns on the highway of life, but time waits for no man. “Don’t miss your exit.”</p>
 				<div class="clearfix"></div><br/>
 				
 				<h3>David G. <small>Freedom</small></h3>
-				<p>A reflection on time lost and the consequence of one mistake many years ago.</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Justino G. <small>Piano Priest</small></h3>
-				<p>The filmmaker muses about his son, a musical prodigy, who uses his gifts to cope with his father’s incarceration.</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Brian H. <small>Dear Time</small></h3>
-				<p>The filmmaker writes to Time, reflecting on loss and gratitude. Does Time heal all wounds?</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Joe K. <small>There He Was, There He Is</small></h3>
-				<p>Go to sleep a child and wake a man. How will yesterday's plans work in tomorrow's society?</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Braheem L. <small>My Boys...</small></h3>
-				<p>Two sons express the challenges of growing up with an incarcerated father.</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Jerome L. <small>Justice</small></h3>
-				<p>Reflecting on dysfunctional communities and growing up in prison,, the filmmaker considers what he has learned and how best to lead others from the dark into the light.</p>
+				<p>The filmmaker reflects on time lost and the consequences of one mistake many years ago.</p>
 				<div class="clearfix"></div><br/>
 				
 				<h3>Lamar L. <small>My Kids</small></h3>
-				<p>Learn from my mistakes, be a leader not a follower.</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Myke L. <small>We Can If We Want To</small></h3>
-				<p>The filmmaker looks at the chaos of the world from behind bars, and suggests it’s time for change. We can make a better world, if we want to.</p>
+				<p>The filmmaker encourages his children to learn from his mistakes and strive to be a leader not a follower.</p>
 				<div class="clearfix"></div><br/>
 				
 				<h3>Paul M. <small>Nostalgia</small></h3>
-				<p>Watching a movie late at night, the filmmaker is reminded of a childhood memory.  He decides to make a phone call...</p>
+				<p>Watching a movie late at night, the filmmaker is reminded of a childhood memory.  He decides to make a phone call.</p>
 				<div class="clearfix"></div><br/>
 				
-				<h3>Donyea P. <small>Big Boy Shoes</small></h3>
-				<p>The young filmmaker looks back on his life and forward to a better future for us all.  It’s time to put on those big boy shoes.</p>
-				<div class="clearfix"></div><br/>
-				
-				<h3>Qwasheem R. <small>Lymph Notes</small></h3>
+				<h3>Qwasheam R. <small>Lymph Notes</small></h3>
 				<p>The filmmaker chronicles his journey through illness and incarceration.</p>
 				<div class="clearfix"></div><br/>
 				
-				<h3>Alexander S. <small>The Bus Stop</small></h3>
-				<p>We all do dumb stuff as kids. We could all use someone watching our back. *Gulp*</p>
+				
+				<br /><br />
+				<div class="text-center">
+					<h2 class="text-underline">Week 2: Criminal Justice Today</h2>
+					<h2>August 22 to August 28, 2019</h2>
+				</div>
+
+				
+				<br /><br />
+				<h3>Filmmakers at Philadelphia’s Riverside Correctional Facility for Women <small>You Can Go Left or Right</small></h3>
+				<p>The filmmakers reflect on life inside a hectic county jail for women.</p>
 				<div class="clearfix"></div><br/>
 				
-				<h3>Clarence S. <small>Through Her, I Live</small></h3>
-				<p>"Angels walk among us."  This is a story of how a life, lost too soon, saved an uncle during his incarceration.</p>
+				<h3>Joseph K. <small>There He Was, There He Is</small></h3>
+				<p>The protagonist goes to sleep a child and wakes a man. How will yesterday's plans work in tomorrow's society?</p>
 				<div class="clearfix"></div><br/>
 				
-				<h3>Joe S. <small>Furever Homes</small></h3>
-				<p>Sometimes you save the dog, sometimes the dog saves you. </p>
+				<h3>Louis B. <small>Money Talks</small></h3>
+				<p>“Money doesn’t grow on trees.” The filmmaker asks, “How much is money really worth?”</p>
 				<div class="clearfix"></div><br/>
 				
 				<h3>Marvin S. <small>Between Two Courts</small></h3>
 				<p>The filmmaker recalls how his youth on the basketball court became a life in criminal court and incarceration.</p>
 				<div class="clearfix"></div><br/>
 				
+				
+				<br /><br />
+				<div class="text-center">
+					<h2 class="text-underline">Week 3: Family and Community Impact</h2>
+					<h2>August 29 to September 4, 2019</h2>
+				</div>
+
+				
+				<br /><br />
+				<h3>Alexander S. <small>The Bus Stop</small></h3>
+				<p>From the filmmaker’s perspective, we all do dumb things as kids, and we could all use someone watching our back.</p>
+				<div class="clearfix"></div><br/>
+				
+				<h3>Braheem L. <small>My Boys...</small></h3>
+				<p>Two sons express the challenges of growing up with an incarcerated father.</p>
+				<div class="clearfix"></div><br/>
+				
+				<h3>Brian H. <small>Dear Time</small></h3>
+				<p>The filmmaker writes to “time,” reflecting on loss and gratitude and wondering if time really heals all wounds.</p>
+				<div class="clearfix"></div><br/>
+				
+				<h3>Clarence S. <small>Through Her, I Live</small></h3>
+				<p>"Angels walk among us." This is a story of how a life, lost too soon, saved an uncle during his incarceration.</p>
+				<div class="clearfix"></div><br/>
+				
+				<h3>Justino G. <small>Piano Priest</small></h3>
+				<p>The filmmaker muses about his son, a musical prodigy, who uses his gifts to cope with his father’s incarceration.</p>
+				<div class="clearfix"></div><br/>
+				
 				<h3>Robert T. <small>A Special Person</small></h3>
 				<p>The filmmaker talks to his young son, reminiscing about the things they share.</p>
 				<div class="clearfix"></div><br/>
 				
-				<h3>Aaron W. <small>Where Did It Go?</small></h3>
-				<p>There are many twist and turns on the highway of life, but time waits for no man. Don’t miss your exit.</p>
+				
+				<br /><br />
+				<div class="text-center">
+					<h2 class="text-underline">Week 4: Restorative Justice</h2>
+					<h2>September 5 to September 11, 2019</h2>
+				</div>
+
+				
+				<br /><br />
+				<h3>Donyea P. <small>Big Boy Shoes</small></h3>
+				<p>The young filmmaker looks back on his life and forward to a better future for us all. “It’s time to put on those big boy shoes.”</p>
 				<div class="clearfix"></div><br/>
 				
+				<h3>Ezra B. <small>Phoenix</small></h3>
+				<p>After a life behind bars, the filmmaker reflects on the cultivation of wisdom and the strength to overcome. “I am the phoenix.”</p>
+				<div class="clearfix"></div><br/>
+				
+				<h3>Jerome L. <small>Justice</small></h3>
+				<p>Reflecting on dysfunctional communities and growing up in prison, the filmmaker considers what he has learned and how best to lead others from the dark into the light.</p>
+				<div class="clearfix"></div><br/>
+				
+				<h3>Joseph S. <small>Furever Homes</small></h3>
+				<p>As the filmmaker says, “Sometimes you save the dog. Sometimes the dog saves you.”</p>
+				<div class="clearfix"></div><br/>
+				
+				<h3>Michael L. <small>We Can If We Want To</small></h3>
+				<p>The filmmaker looks at the chaos of the world from behind bars and suggests it’s time for change. We can make a better world if we want to.</p>
+				<div class="clearfix"></div><br/>
+				
+				
+				<br /><br />
+				<div class="text-center">
+					<h2 class="text-underline">Finale – September 12, 2019</h2>
+				</div>
+
+				
+				<br /><br />
+				<p>All 20 films listed above will be screened.</p>
+				<div class="clearfix"></div><br/>
 			</div>
 		</div>
 	</div>
